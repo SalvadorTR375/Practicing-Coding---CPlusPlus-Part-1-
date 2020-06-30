8f8e929722a2bfff384e839bda355f9313bf92ef
Salvador Torres Rodriguez <a00825038@itesm.mx>
2020-06-30 14:05:07 -0500

    pruebas
    ...

---
+++ test02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << " Hola usuario, cuantos anos tienes? " << endl; 
+    int edad = 0; 
+    cin >> edad;
+    cout << "tienes estos anos -->" << edad << endl;
+    return 0;
+}
